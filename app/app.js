186c72bbf0251c8856565db0a6ccfcd3191eee97
Pratheesh <pratheeshrajan18@gmail.com>
2017-11-12 13:30:47 +0530

    Combined commit

---
+++ app/app.js
@@ -0,0 +1,16 @@
+import Application from '@ember/application';
+import Resolver from './resolver';
+import loadInitializers from 'ember-load-initializers';
+import config from './config/environment';
+
+const App = Application.extend({
+  modulePrefix: config.modulePrefix,
+  podModulePrefix: config.podModulePrefix,
+  Resolver
+});
+
+console.log("===i am test===");
+
+loadInitializers(App, config.modulePrefix);
+
+export default App;
