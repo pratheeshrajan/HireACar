60d9e9cc19386ec63c47cac8ed8091a4ba727190
Pratheesh <pratheeshrajan18@gmail.com>
2017-11-12 13:45:26 +0530

    new combined commit
    removed the console
    
    added the console

--- app/app.js
+++ app/app.js
@@ -3,14 +3,12 @@ import Application from '@ember/application';
 import Resolver from './resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
-
+console.log("new console");
 const App = Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
   Resolver
 });
-
-console.log("===i am test===");
 
 loadInitializers(App, config.modulePrefix);
 
